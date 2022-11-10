1a8e27be17db372890a559b4442ec877dc70820b
Jack McKinstry <jack.mckinstry@colorado.edu>
2022-11-10 00:16:36 -0700

    Completed nurse page with calls to SQL database for loading patient info, switching between them, and posting updates on patients to the database

--- project_components/index.js
+++ project_components/index.js
@@ -110,77 +110,82 @@ app.get('/patientInfo', (req, res)=> {
 })
 
 // global variable for the nurse's currently selected patient
+let nurseLoggedInID = 1; // TODO temp, remove or update based on login
 let currentPatientID = -1;
 
 app.get('/nurse', (req, res) => {
   // TODO: require nurse perm level for viewing this page, else send a message that user has no access
 
-  // const query = "select * from users";
-  // db.one(query)
-  //   .then((data) => {
-  //     console.log(data);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  // TODO: database call to get info prior to render
-  
-  if (currentPatientID === -1) {
-    currentPatientID = 1; 
-    // TODO: this should default to the nurse's lowest patient id from db query call
-  }
+  const patientsQuery = `SELECT id, legal_name FROM users 
+    JOIN patient_to_nurse ON users.id = patient_to_nurse.patient_id
+    WHERE patient_to_nurse.nurse_id = ${nurseLoggedInID};`;
 
-  // TODO load data for the patient with query based on currentPatientID field
-  const data = {
-    selected_id: `${currentPatientID}`,
-    nurse_patients: [
-      {
-        patient_id: "1",
-        patient_name: "John Snow"
-      },
-      {
-        patient_id: "2",
-        patient_name: "Jane Doe"
-      }   
-    ],
-    patient_name: "John Snow",
-    dob: "2012-04-23T18:25:43.511Z",
-    patient_needs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    visits: [
-      {
-        nurse: "Jan Smith",
-        date: "11/01/2022",
-        time: "Morning",
-        notes: "Seemed happy, took medication. Discussed how their weekend went."
-      },
-      {
-        nurse: "Jan Smith",
-        date: "11/01/2022",
-        time: "Evening",
-        notes: "Consistent mood with morning, but noticably fatigued. Night medication was taken."
+    // this should not be multiple nested queries, it should use async and await and tasks
+    // while this is bad practice, it works (for now).
+  db.any(patientsQuery)
+    .then((allPatients) => {
+      // to get info for all patients of the nurse
+      
+      if (currentPatientID === -1) {
+        currentPatientID = allPatients[0].id;
+        // this should default to the nurse's lowest patient id from the db query call
       }
-    ],
-    medication: [
-      {
-        name: "Scary Drug Name",
-        dose: "30mg",
-        frequency: "Twice Daily",
-      },
-      {
-        name: "Youth Serum",
-        dose: "5000mg",
-        frequency: "Once in morning",
-      },
-      {
-        name: "Happy pills",
-        dose: "20mg",
-        frequency: "As needed"
-      }
-    ]
-  }
 
-  res.render("pages/nurse", {data});
+      console.log("allPatients:");
+      console.log(allPatients);
+      // all patients
+      console.log("Current patient ID:");
+      console.log(currentPatientID);
+
+      // THEN queryB to get info for current patient
+      const currPatientQuery = `
+        SELECT * FROM users
+        WHERE users.id = ${currentPatientID};`;
+      const medicationsQuery = `
+        SELECT medication.* FROM users
+        JOIN patient_to_medication ON users.id = patient_to_medication.patient_id
+        JOIN medication ON patient_to_medication.medication_id = medication.medication_id
+        WHERE users.id = ${currentPatientID};`;
+      const visitsQuery = `
+        SELECT visit.* FROM users
+        JOIN patient_to_visit ON users.id = patient_to_visit.patient_id
+        JOIN visit ON patient_to_visit.visit_id = visit.visit_id
+        WHERE users.id = ${currentPatientID}
+        ORDER BY visit.visit_id DESC;`;
+      db.any(currPatientQuery)
+        .then((currPatient) => {
+          console.log("Curr Patient:");
+          console.log(currPatient);
+          
+          db.any(medicationsQuery)
+            .then((medications) => {
+              console.log("Medications:");
+              console.log(medications);
+              
+              db.any(visitsQuery)
+                .then((visits) => {
+                  console.log("Visits:");
+                  console.log(visits);
+                  
+                  res.render("pages/nurse", {allPatients, currPatient, medications, visits});
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
 
 app.post('/selectpatient', (req,res) => {
   let select_id = parseInt(req.body.selected_patient);
@@ -191,5 +196,26 @@ app.post('/selectpatient', (req,res) => {
 });
 
 app.post('/patientupdate', (req,res) => {
-  // TODO, for nurse patient update form
+  var updateIn = req.body.updateInput;
+  var currentTime = new Date();
+  var currentTimeFormatted = currentTime.toISOString();
+  var query = `
+  INSERT INTO visit(nurse_id, notes, date)
+  VALUES (${nurseLoggedInID}, '${updateIn}', '${currentTimeFormatted}');
+  
+  INSERT INTO patient_to_visit(patient_id, visit_id)
+  VALUES (
+      ${currentPatientID}, 
+      (SELECT visit.visit_id FROM visit ORDER BY visit.visit_id DESC LIMIT 1) 
+    );`;
+  db.any(query)
+    .then(function (rows) {
+      // TODO send message saying update has been posted
+      console.log("Patient update posted");
+      console.log(`${updateIn}`);
+      res.redirect("/nurse");
+    })
+    .catch(function (error) {
+      res.send({'message' : error});
+    });
 });
